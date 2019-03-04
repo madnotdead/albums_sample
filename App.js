72463225aa218dcaf013510015f3c816f20aad4c
Leandro <leandrom@lagash.com>
2019-03-03 22:48:38 -0300

    app finished

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, View} from 'react-native';
+import Header from './src/components/header'
+import AlbumList from './src/components/AlbumList'
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header headerText={'Albums'}/>
+        <AlbumList/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
